2c8b5c266b56d46abad47ba962bf983843ab4b74
mafiti91 <ppivankov@gmail.com>
2022-05-23 22:53:12 +0300

    feat: add solution task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,63 +1,29 @@
-/*const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => { 
-  if (err) console.log(err); 
-});
+  if (err) throw err; }  
+);
 
 fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
-  if(err) console.log(err);
+  if(err) throw err;
   files.forEach((file) => {
     fs.unlink(path.join(__dirname, 'files-copy', `${file}`), (err) => {
-      if (err) console.log(err);      
+      if (err) throw err;      
     });
   })
 });
 
 fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-  if(err) console.log(err);
+  if(err) throw err;
   files.forEach((file) => {
     fs.writeFile(path.join(__dirname, 'files-copy', `${file}`), '', (err) => { 
-      if(err) console.log(err);       
+      if(err) throw err;       
     });
     fs.copyFile(
       path.join(__dirname, 'files', `${file}`), 
       path.join(__dirname, 'files-copy', `${file}`), 
       (err) => {
-        if(err) console.log(err) });
+        if(err) throw err; });
   }) 
-});*/
-
-
-
-const fs = require('fs');
-const path = require('path');
-
-
-fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-    if (err) console.log(err);        
 });
-
-fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
-  if(err) console.log(err);
-    files.forEach((file) => {
-      fs.unlink(path.join(__dirname, 'files-copy', `${file}`), (err) => {
-        if (err) console.log(err);      
-      });
-    })
-
-});
-
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-if(err) console.log('error');
-    files.forEach((file) => {
-        fs.writeFile(
-            path.join(__dirname, 'files-copy', `${file}`), '',
-            (err) => { if (err) throw err;       
-            }
-        );
-        fs.copyFile(
-            path.join(__dirname, 'files', `${file}`), 
-            path.join(__dirname, 'files-copy', `${file}`), err => {if(err) console.log('fac1', err) })
-    }) 
-})
